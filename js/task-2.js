f043b78b499345e68512c3e4f6c618c12ffec607
Andriy Kikot <andray838@gmail.com>
2020-07-29 21:03:05 +0300

    fixed after cheсk

--- js/task-2.js
+++ js/task-2.js
@@ -18,22 +18,17 @@ const inventory = {
 
 const invokeInventoryAction = function (itemName, action) {
     console.log(`Invoking action on ${itemName}`);
-
-    // ver.1
-    // const actionWithInventory = action.bind(inventory);
-    // actionWithInventory(itemName);
-
-    action.bind(inventory)(itemName);
+    action(itemName);
 };
 
 
-invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
